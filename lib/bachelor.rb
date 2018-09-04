a3807951e52713113aa712d8f265c844ae4ffb42
srscalia <shelby.scalia@gmail.com>
2018-09-04 12:22:59 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,90 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = 0
+  data.each do |s, contestants|
+    if s==season
+      contestants.each do |person|
+        person.each do |attribute, value|
+          if attribute == "status" && value == "Winner"
+            first_name = person["name"].split[0]
+          end
+        end
+      end
+    end
+  end
+  first_name
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  first_name = 0
+  data.each do |s, contestants|
+    contestants.each do |person|
+      person.each do |attribute, value|
+        if value == occupation
+          first_name = person["name"]
+        end
+      end
+    end
+  end
+  first_name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |s, contestants|
+    contestants.each do |person|
+      person.each do |attribute, value|
+        if attribute == "hometown" && value==hometown
+          counter+=1
+        end
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation=0
+  data.each do |s, contestants|
+    contestants.find do |person|
+      if person["hometown"]==hometown
+        occupation = person["occupation"]
+      end
+    end
+  end
+  occupation
 end
 
+#def get_occupation(data, hometown)
+#  occupation=0
+#  data.each do |s, contestants|
+#    contestants.each do |person|
+#      person.each do |attribute, value|
+#        if value == hometown
+#          occupation = person["occupation"]
+#          binding.pry
+#        end
+#      end
+#    end
+#  end
+#  occupation
+#end
+
 def get_average_age_for_season(data, season)
-  # code here
+  sum=0
+  count = 0
+  data.each do |s, contestants|
+    if s == season
+      contestants.each do |person|
+        person.each do |attribute, value|
+          if attribute == "age"
+            sum+=value.to_i
+            count+=1
+          end
+        end
+      end
+    end
+  end
+  (sum/count.to_f).round
 end
